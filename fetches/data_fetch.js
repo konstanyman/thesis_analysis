73893d348b63f94ead2c0cb7834250780fc1e595
konstanyman <80118317+konstanyman@users.noreply.github.com>
2025-02-26 14:49:04 +0200

    added event types and time and distance of players

--- fetches/data_fetch.js
+++ fetches/data_fetch.js
@@ -18,7 +18,7 @@ async function fetchTournamentData(tournamentIds) {
     const timelinePromises = matches.map(({ tournamentId, matchId }) => 
         limit(async () => {
             const timeline = await fetchData(
-            `   ${baseUrl}/${tournamentId}/matches/${matchId}/timeline?eventTypes=Shot,Pass,BallContest,Dribble,HeaderContest`
+            `   ${baseUrl}/${tournamentId}/matches/${matchId}/timeline?eventTypes=Shot,Pass,BallContest,Dribble,HeaderContest,ThrowIn,CornerKick,KickOff,GoalKick,FreeKick,PenaltyKick`
             );
 
             // Ensure we attach tournamentId & matchId
